d1be3c0be20c6dd1ae1059b2a0dca6f5189377db
MarcoRosenzweig <marco.ros@hotmail.de>
2020-06-21 16:16:22 +0200

    Adjusted notebooks and scripts to most recent changes.

--- CRYPTO_MODEL_AUTOGEN.py
+++ CRYPTO_MODEL_AUTOGEN.py
@@ -1,34 +1,51 @@
 #!/usr/bin/env python
 # coding: utf-8
 
+# # FROM TERMINAL, RUN THIS LINE:
+# jupyter nbconvert --execute --to html CRYPTO_MODEL_AUTOGEN.ipynb
+
+# In[ ]:
+
+
+get_ipython().run_cell_magic('javascript', '', 'IPython.OutputArea.auto_scroll_threshold = 9999')
+
+
+# In[ ]:
+
+
 from model import MODEL
-import utils, plotting, statistics
+import utils, plotting, fstats
 import pandas as pd
 
 
 # In[ ]:
 
 
-start_date = '2020-01-01' #your start date for the model.
-day_hour = 18 #your intra-day time at which to evaluate the model.
-tickers = 'BTC-USD'
+#your intra-day time at which to evaluate the model.
+day_hour = 18
+#your tickers of interest
+tickers = ['BTC-USD']
 
 
 # In[ ]:
 
 
+#do not edit below this cell!
+
+
+# In[ ]:
+
+
+#specify dates:
+todays_date = pd.Timestamp.today()
+start_date = todays_date - pd.Timedelta('200 days')
+filter_date = start_date.floor(freq='D').replace(hour=day_hour)
+#get data:
 model = MODEL(tickers=tickers)
 model.get_data(start=start_date, interval='60m')
-model.data.tail(2)
-
-
-# In[ ]:
-
-
-start_date_range = pd.Timestamp(2020, 1, 1, day_hour)
-date_range = utils.create_date_range(start_date=start_date_range)
+#filter by datetime:
+date_range = utils.create_date_range(start_date=filter_date)
 model.apply_date_filter(date_range, force_apply=True)
-model.data.tail()
 
 
 # In[ ]:
@@ -40,7 +57,14 @@ model.eval_model()
 # In[ ]:
 
 
-plotting.plot_model(model, tickers='BTC-USD', plot_from_date='2020-04-01')
+plot_date = todays_date - pd.Timedelta('30 days')
+plot_start = str(plot_date.date())
+
+
+# In[ ]:
+
+
+plotting.plot_model(model, tickers='BTC-USD', plot_from_date=plot_start)
 
 
 # In[ ]:
@@ -55,11 +79,11 @@ imag_model._init_model()
 # In[ ]:
 
 
-imag_model.show_possibilities(plot_from_date='2020-05-04', switch_axes=False)
+imag_model.show_possibilities(plot_from_date=plot_start, switch_axes=False)
 
 
 # In[ ]:
 
 
-statistics.calc_probs(model=imag_model, tickers='all', auto_update_tolerances=True)
+fstats.calc_probs(model=imag_model, tickers='all', auto_update_tolerances=True)
 
