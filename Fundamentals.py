84c60e0a44e811185985b412bb57d9fe70850b0b
Patrick Lorenz <61086523+ptlorenz@users.noreply.github.com>
2020-09-06 22:01:27 +0200

    Added 50 Day MA & Todays Price vs MA

---
+++ Fundamentals.py
@@ -0,0 +1,96 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+import pandas as pd
+import numpy as np
+import datetime
+import yfinance as yf
+import matplotlib.pyplot as plt
+import seaborn as sns
+import itertools
+
+
+OUTPUT_FOLDER = "Option_Weekly_csv"
+import os
+try:
+    os.mkdir(OUTPUT_FOLDER)
+except FileExistsError:
+    pass
+
+def fundamentals (Company_Names, output_folder=OUTPUT_FOLDER, export_table=True):
+
+    C_N_MarketCap = []
+    C_N_Beta = []
+
+    for x in Company_Names:
+        try:
+            ticker = yf.Ticker(x)
+            info_dic = ticker.info
+            var1 = info_dic['marketCap']/1000000000
+            var2 = info_dic['beta']
+            print(x, " Downloaded")
+            C_N_MarketCap.append(var1)
+            C_N_Beta.append(var2)
+
+        except IndexError:
+            C_N_MarketCap.append("I E")
+            C_N_Beta.append("I E")
+            continue
+        except TypeError:
+            C_N_MarketCap.append("T E")
+            C_N_Beta.append("T E")
+            continue
+
+    year_pct_change_list = []
+    data_vol_list = []
+    moving_averages_list = []
+    price_vs_ma_list = []
+    todaydate = datetime.date.today()
+
+    for x in Company_Names:
+
+        data = yf.download(x, start= todaydate - datetime.timedelta(days=50))
+        data_c = data["Close"]
+        data_beg = data_c[-7]
+        data_end = data_c[-1]
+        data_ma_prices = np.sum(data_c)
+        data_ma_days = np.shape(data_c)
+        #Calculate Return
+        year_pct_change = (data_end-data_beg)/data_beg*100
+        year_pct_change_list.append(year_pct_change)
+        moving_average = float(data_ma_prices / data_ma_days)
+        moving_averages_list.append(moving_average)
+        price_vs_ma_pct = (moving_average - data_end)/data_end*100
+        price_vs_ma_list.append(price_vs_ma_pct)
+        #Calculate Volatility
+        data_change = np.diff(data_c)
+        data_pctchange = data_change/data_c[:-1]*100
+        data_vol = data_pctchange.std()
+        data_vol_list.append(data_vol)
+
+    Fundamentals_X_input = pd.DataFrame({"Tickers":Company_Names,"MarketCap":C_N_MarketCap,"Beta":C_N_Beta, "50 Day Moving Average": moving_averages_list,"MA to TP %":price_vs_ma_list, "1W %Change":year_pct_change_list, "30day Volatility":data_vol_list})
+
+    data = yf.download("SPY", start= todaydate - datetime.timedelta(days=7))
+    benchmark = data["Close"]
+    benchmark_beg = benchmark[0]
+    benchmark_end = benchmark[-1]
+    #benchmark_pct_change as effect on option prediction & predicted price
+    benchmark_1week_pct_change = (benchmark_end-benchmark_beg)/benchmark_beg*100
+    benchmark_pct_change_list = [benchmark_1week_pct_change]*np.shape(year_pct_change_list)[0]
+
+    Fundamentals_X_input["1W Ticker vs Benchmark %Change"] = Fundamentals_X_input["1W %Change"] -    benchmark_pct_change_list
+
+    todaydate = datetime.date.today()
+    today_str = str(todaydate).replace("-", "_")
+    try:
+        folder = os.path.join(output_folder, today_str)
+        os.mkdir(folder)
+    except FileExistsError:
+        pass
+
+    table_name = os.path.join(folder, "Company_Fundamentals_{}.csv".format(today_str))
+    if export_table:
+        Fundamentals_X_input.to_csv(table_name)
+        print("Exported: {}".format(table_name))
+
+    return Fundamentals_X_input
